39f13a96dfb1d30d04267679bfe9092bbd68efd9
Deepeshvyas2004 <145889405+Deepeshvyas2004@users.noreply.github.com>
2026-03-20 16:39:05 +0530

    Add files via upload

---
+++ Authentication/jwt_authentication_final/jwt_fixed/user_service/src/main/java/com/project_management_system/user_service/dto/LoginRequestDto.java
@@ -0,0 +1,15 @@
+package com.project_management_system.user_service.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class LoginRequestDto {
+
+    @NotBlank(message = "Username is required")
+    private String username ;
+
+    @NotBlank(message ="Password is required")
+    private String password ;
+
+}
